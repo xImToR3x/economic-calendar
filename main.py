aff4be8a937a3856eec09ba207c201803804976d
xImToR3x <108129787+xImToR3x@users.noreply.github.com>
2025-06-25 20:34:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return jsonify({"message": "Hello from Python on Render!"})
 
-# Optional: weitere Routen z.B. /economic-calendar, /btc-price
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
